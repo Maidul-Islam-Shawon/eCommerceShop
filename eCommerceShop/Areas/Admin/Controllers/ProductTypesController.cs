84c37dbb4dba554ba40d09629f468f05475e7d32
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2021-02-09 05:19:58 +0600

    Add project files.

---
+++ eCommerceShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            var allProductTypes = _db.ProductTypes.ToList();
+            
+            return View(allProductTypes);
+        }
+    }
+}
