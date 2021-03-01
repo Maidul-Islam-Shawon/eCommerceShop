d9251c2d09da8454ff40b7f17335842893763f1b
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2021-03-02 03:40:59 +0600

    New Test Added.

--- eCommerceShopNUnitTest/ProductServicesTest.cs
+++ eCommerceShopNUnitTest/ProductServicesTest.cs
@@ -81,5 +81,142 @@ namespace eCommerceShopNUnitTest
 
             Assert.That(filterProduct, Is.EqualTo(products.Where(m => m.Price >= 100 && m.Price <= 500)));
         }
+
+        [Test]
+        public void FilterByMinMaxAmount_ByEqual()
+        {
+            //Arange
+            var minAmount = 100;
+            var maxAmount = 500;
+
+            var products = new List<Products>()
+            {
+                new Products
+                {
+                    Name="Iphone X",
+                    Price=200,
+                    ProductColor="Red",
+                    IsAvailable=true,
+                    ProductTypesId=1,
+                    SpecialTagId=2
+                },
+                new Products
+                {
+                    Name="Logitech Keyboard",
+                    Price=500,
+                    ProductColor="Black",
+                    IsAvailable=true,
+                    ProductTypesId=2,
+                    SpecialTagId=3
+                },
+                new Products
+                {
+                    Name="Skullcandy Headphone",
+                    Price=450,
+                    ProductColor="Blue",
+                    IsAvailable=true,
+                    ProductTypesId=4,
+                    SpecialTagId=1
+                },
+                new Products
+                {
+                    Name="Samsung Tv 32",
+                    Price=1450,
+                    ProductColor="Black",
+                    IsAvailable=true,
+                    ProductTypesId=1,
+                    SpecialTagId=1
+                },
+                new Products
+                {
+                    Name="Samsung D900",
+                    Price=120,
+                    ProductColor="Silver",
+                    IsAvailable=false,
+                    ProductTypesId=5,
+                    SpecialTagId=1
+                }
+            };
+
+            //Act
+
+            var filterProduct = productServices.FilterByMinMaxAmount(minAmount, maxAmount, products);
+
+            var expectedResult = products.Where(m => m.Price >= minAmount && m.Price <= maxAmount);
+
+            //Assert
+
+            //Assert.That(filterProduct, Is.EqualTo(products.Where(m => m.Price >= 100 && m.Price <= 500)));
+            Assert.AreEqual(expectedResult, filterProduct);
+            
+        }
+
+        [Test]
+        public void FilterByMinMaxAmount_Wrong()
+        {
+            //Arange
+            var minAmount = 100;
+            var maxAmount = 500;
+
+            var products = new List<Products>()
+            {
+                new Products
+                {
+                    Name="Iphone X",
+                    Price=200,
+                    ProductColor="Red",
+                    IsAvailable=true,
+                    ProductTypesId=1,
+                    SpecialTagId=2
+                },
+                new Products
+                {
+                    Name="Logitech Keyboard",
+                    Price=500,
+                    ProductColor="Black",
+                    IsAvailable=true,
+                    ProductTypesId=2,
+                    SpecialTagId=3
+                },
+                new Products
+                {
+                    Name="Skullcandy Headphone",
+                    Price=450,
+                    ProductColor="Blue",
+                    IsAvailable=true,
+                    ProductTypesId=4,
+                    SpecialTagId=1
+                },
+                new Products
+                {
+                    Name="Samsung Tv 32",
+                    Price=1450,
+                    ProductColor="Black",
+                    IsAvailable=true,
+                    ProductTypesId=1,
+                    SpecialTagId=1
+                },
+                new Products
+                {
+                    Name="Samsung D900",
+                    Price=120,
+                    ProductColor="Silver",
+                    IsAvailable=false,
+                    ProductTypesId=5,
+                    SpecialTagId=1
+                }
+            };
+
+            //Act
+
+            var filterProduct = productServices.FilterByMinMaxAmount(minAmount, maxAmount, products);
+
+            var expectedResult = products.Where(m => m.Price >= minAmount);
+
+            //Assert
+
+            //Assert.That(filterProduct, Is.EqualTo(products.Where(m => m.Price >= 100 && m.Price <= 500)));
+            Assert.AreNotEqual(expectedResult, filterProduct);
+        }
     }
 }
